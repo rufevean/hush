8c0384fba460b500af85a6530f80d03bda7d25b2
rufevean <deerajmydearalways@gmail.com>
2022-11-05 10:58:08 +0530

    Updated Readme file

--- bot.py
+++ bot.py
@@ -1,11 +1,24 @@
+#importing all the requried stuff
 import discord
-from datetime import datetime
-hush = discord.Client(intents=discord.Intents.default())
+from discord.ext import commands
+import asyncio
+from datetime import datetime ,time, timedelta
+#giving a prefix to the bot and creating it
+hush = commands.Bot(command_prefix="*",intents=discord.Intents.default())
+#giving a time to send the message
 noTime = datetime.now()
 time = noTime.hour
+channel_id = 1038026099797528646
+#for logs
 @hush.event
 async def on_ready():
     print("the bot is ready")
+
+# async def on_ready():
+#     # await bot.wait_until_ready()
+#     channel = bot.get_channel(channel_id)
+#     await channel.send("good day eh?")
+
 
 @hush.event
 async def on_message(message):
@@ -14,7 +27,7 @@ async def on_message(message):
 
 @hush.event
 async def on_message(message):
-    if message.content.startswith('$hello'):
-        await message.channel.send("hello world")
-
-hush.run('MTAzODAyNTI2ODQyNzc2MzcxMg.GyQt9b.XI5gwYf4492OoVIuJGtpaL8WSnHntkRN5R6_oo')
+    if message.content == 'hello':
+        await message.channel.send('hello world')
+if __name__ == "__main__":
+    hush.run('MTAzODAyNTI2ODQyNzc2MzcxMg.GOs6Lz.uBqZcK6hVF6r6Y4aFOVmj-Up-F9joA6ltqePqY')
