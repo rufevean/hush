e83cf845eb5ac095d02618722d1b8bfc01e7bd62
rufevean <deerajmydearalways@gmail.com>
2022-11-05 20:40:08 +0530

    few changes added

--- bot.py
+++ bot.py
@@ -1,5 +1,6 @@
 #importing all the requried stuff
 import discord
+import json
 from discord.ext import commands
 import asyncio
 from datetime import datetime ,time, timedelta
@@ -7,24 +8,16 @@ from datetime import datetime ,time, timedelta
 bot = commands.Bot(command_prefix="*", intents=discord.Intents.all())
 #giving a time to send the message
 noTime = datetime.now()
-token  = open("token.txt", "r").read()
-# time = noTime.hour
-channel_id = 1038026099797528646
+with open('token.json') as f:
+    data = json.load(f)
+    token = data["token"] # reads the token from another file
 #for logs
 @bot.event
 async def on_ready():
     print("the bot is ready") #for our logs to check if the bot is started
+    if noTime.hour==20:#checks if the time is 7 pm and then proceds to send a text file
+       channel = bot.get_channel(1038026099797528646) #grabbing the test channel
+       await channel.send("hello")
 
-
-@bot.event
-async def on_message(message):
-    if message.author.bot: #doesnt reply to bots
-        return
-    message1 = "*time"
-    message2 = "*hello"
-    if message.content == message1: #checks if the message is message1
-        await message.reply("wait") #replies with wait
-    elif message.content == message2: #checks if the message is message2
-        await message.reply("hello bot") #rplies with the hello bot
 if __name__ == "__main__":
     bot.run(token) #starting the bot
